6ea9d4b63b0ecefce97487ed1b7c4e61db738fe6
David <david.lourenco94@gmail.com>
2022-07-07 18:00:15 -0300

    Terceiro Requisito Ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
+    // handleclick = () => {
+
+    // };
+
     return (
       <div>
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
       </div>
     );
   }
